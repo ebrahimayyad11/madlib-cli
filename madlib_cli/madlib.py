8edfbe370cb8798956ccc2ad96de4f37b4dd41e3
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-06-12 16:08:25 +0300

    doing lab-03

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,61 +1,47 @@
 import re
 
-def welcome():
-    print("""Hello, welcome to our game
-    Insert an example of the language parts""")
+def welcome_print():
+    print(""" 
+    Welcome to madlib game!
+    all you need is to think of an example of the below vocabs 
+    """)
 
-data = ['Adjective','Adjective','A First Name','Past Tense Verb','A First Name','Adjective','Adjective',
+data=['Adjective','Adjective','A First Name','Past Tense Verb','A First Name','Adjective','Adjective',
 'Plural Noun','Large Animal','Small Animal',"A Girl's Nam",'Adjective','Plural Noun','Adjective','Plural Noun',
 'Number 1-50',"First Name's",'Number','Plural Noun','Number','Plural Noun']
 
+new_data=[]
 
-newData = []
-
-def insertData():
-    for i in range(len(data)):
-        newInput = input("Insert "+data[i]+"\n")
-        newData.append(newInput)
-
+def input_vocabs():
+    for i in range (len(data)):
+        input_val=input('>> Enter %s  '%(data[i]))
+        new_data.append(input_val)
 
 
-# if __name__ == '__main__':
-#     welcome()
-#     # insertData()
-
+if __name__== '__main__':
+    welcome_print()
+    input_vocabs() 
 
 
 def read_template(path):
     try:
         with open(path) as file:
-          return file.read()
-    except FileNotFoundError:
-        print('error!')
+            return file.read()
+    except FileNotFoundError :
+        raise FileNotFoundError('The file not found')
+   
 
-readData = read_template("./assets/dark_and_stormy_night_template.txt")
-
-
-
-def parse_template(data):
-    modifiedData=re.sub('{[^}]+}','{}',data)
-    removedData=re.findall('{(.*?)\}',data)
-    return  modifiedData, removedData
+def parse_template(read_script):
+    modified_script=re.sub('{[^}]+}','{}',read_script)
+    removed_str_parts=tuple(re.findall("{(.*?)}",read_script))
+    return  modified_script, removed_str_parts
 
 
-parseData = parse_template(readData)
-# parseData = parseData[0]
+def merge(parsed_script,user_input):
+    return parsed_script.format(*user_input)
 
 
 
-def merge(parseData,newData):
-    return parseData.format(*newData)
-    
-
-def copy():
-    with open('./assets/new_assets.txt', 'wb') as scriptWrite:
-         return scriptWrite.write(bytes(merge(parseData,newData),'utf-8'))
-
-# copy()
-
-
-
-
+def script_copy(merged_script):
+    with open('./assets/new_assets.txt','wb') as script_write:
+         return script_write.write(bytes(merged_script,'utf-8'))
